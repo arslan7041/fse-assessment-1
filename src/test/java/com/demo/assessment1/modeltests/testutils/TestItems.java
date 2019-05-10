2e0878e4fd5b2224665025d92978eb8c0b6b6833
Ray Hedgecock <raymond.hedgecock@cognizant.com>
2019-05-10 10:15:16 -0400

    added to list

--- src/test/java/com/demo/assessment1/modeltests/testutils/TestItems.java
+++ src/test/java/com/demo/assessment1/modeltests/testutils/TestItems.java
@@ -1,6 +1,7 @@
 package com.demo.assessment1.modeltests.testutils;
 
 import com.demo.assessment1.models.LineItem;
+import com.demo.assessment1.models.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,13 @@ import java.util.List;
 public class TestItems {
 
     public static List<LineItem> getLineItems(){
-        List<LineItem> products = new ArrayList<LineItem>();
+        List<LineItem> lineitems = new ArrayList<LineItem>();
+
+        lineitems.add(new LineItem(new Product("shoes", 50), 2));
+        lineitems.add(new LineItem(new Product("pants", 100), 1));
+        lineitems.add(new LineItem(new Product("shirt", 80), 5));
+        lineitems.add(new LineItem(new Product("bag", 150), 1));
+
+
     }
 }
